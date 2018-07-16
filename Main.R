6952dd069f41ebd552aeffd9d2f58113d9f45ae0
DESKTOP-TTSK634\PILAR <pilu_369@hotmail.com>
2018-07-16 18:37:10 +0200

    commit

--- Main.R
+++ Main.R
@@ -7,10 +7,6 @@ library("keras")
 library("tensorflow")
 library("reticulate")
 
-#K <- keras::backend()
-
-
-
 # ####################
 # CARGA Y PREPARACION DE DATOS DE DATOS
 # ####################
@@ -20,37 +16,48 @@ library("reticulate")
 #' LEER CSV
 
 Datos <- read.table(file="datos_campus_virtual.txt",header=TRUE)
- 
+
+# convertido a datos categoricos
+# for(unique_value in unique(Datos$feno)){
+#   Datos[paste("feno", unique_value, sep = ".")] <- ifelse(Datos$feno == unique_value, 1, 0)
+# }
+
+# for(unique_value in unique(Datos$grado)){
+#   Datos[paste("grado", unique_value, sep = ".")] <- ifelse(Datos$grado == unique_value, 1, 0)
+# }
+
+
 #' PRUNE THE DATA
 #' En el caso de que existas datos no relevantes o registros con valores nulos
 #' PRUNE THE DATA
 
 
 # Información del conjunto de datos
-#summary(Datos)
+summary(Datos)
 
 # Metedo de validacion interna holdout 
 # Creacion de subconjutnos de entrenamiento (334) y test (166)
 Division <- holdout(y=Datos$recid)
 
-# Ya el metodo de partición da los indices del objeto se deben obtener dichos registros de los indices
-x_train <- Datos[Division$tr,]
-x_test <- Datos[Division$ts,]
+# Obtener conjuntos X,Y de test y entrenamiento
+train <- Datos[Division$tr,]
+x_train <- subset.data.frame(train,select = -recid)
+y_train <- subset.data.frame(train,select = recid)
+
+test <- Datos[Division$ts,]
+x_test <- subset.data.frame(test,select = -recid)
+y_test <- subset.data.frame(test,select = recid)
+
 
 # Comvertirlo en tipo matriz
 x_train_matrix = data.matrix(x_train)
+y_train_matrix = data.matrix(y_train)
+
 x_test_matrix = data.matrix(x_test)
+y_test_matrix = data.matrix(y_test)
 
 print(cat('Conjunto de entrenamiento. Número de columna: ',ncol(x_train_matrix),' Número de filas: ',nrow(x_train_matrix)))
 print(cat('Conjunto de test Número de columna: ',ncol(x_test_matrix),' Número de filas: ',nrow(x_test_matrix)))
-
-
-# Esto sería como en el ejemplo, pero no entiendo para que lo hace 
-#x_train_2 <- x_train_matrix %>% apply(1, as.numeric) %>% t()
-#x_test_2 <- x_test_matrix %>% apply(1, as.numeric) %>% t()
-
-#print(cat('Conjunto de entrenamiento 2. Número de columna: ',ncol(x_train_2),' Número de filas: ',nrow(x_train_2)))
-#print(cat('Conjunto de test 2. Número de columna: ',ncol(x_test_2),' Número de filas: ',nrow(x_test_2)))
 
 
 # ####################
@@ -59,35 +66,36 @@ print(cat('Conjunto de test Número de columna: ',ncol(x_test_matrix),' Número de
 
 # Variables de inicio
 
-# Porque el número de columnas del conjunto de variables es 8
-original_dim <- 8L #334L
+# Porque el número de columnas del conjunto de variables es 7
+original_dim <- 7L 
 # Reducir el número en la capa de codificación
-encoding_dim <- 4L #32L
+encoding_dim <- 4L 
 # Número de neurona de salida
-latent_dim <- 8L #334L
+latent_dim <- 7L 
 
 # **********************
 # Definicion del modelo 
 # **********************
 
 # this is our input placeholder
-# Se establece la entrada como un vector con un numero de neuronar de original_dim = 784L
-input_img <- layer_input(shape = c(original_dim))
+# Se establece la entrada como un vector con un numero de neuronas de original_dim
+input <- layer_input(shape = c(original_dim))
 
 # "encoded" is the encoded representation of the input
-# La capa de codificación, donde se le pasa la capa de entrada, dimesion (numero de neuronas), funcion de activacion (Rectificador (redes neuronales))
-encoded<- layer_dense(input_img,encoding_dim , activation = "relu")
+# La capa de codificación, donde se le pasa la capa de entrada, dimesion (numero de neuronas), funcion de activacion
+encoded<- layer_dense(input,encoding_dim , activation = "relu")
 
-# "decoded" is the lossy reconstruction of the input
-# La capa de decodificacion, vuelve a tener las dimensiones de la entrada existe una perdida de informacion con respecto a la entreda
-# Como prámetros se utiliza la salida de la capa de codificacion,tamaño de la capa de decodificacion (latent_dim), funcion de activcion (sigmoid) 
+#Generamos el modelo de condificacion
+model_enconded <- keras_model(input, encoded)
+
+# La capa de decodificacion, vuelve a tener las dimensiones de la entrada existe una perdida de informacion con respecto a la entrada
+# Como prámetros se utiliza la salida de la capa de codificacion,tamaño de la capa de decodificacion (latent_dim), funcion de activacion (sigmoid) 
 decoded<- layer_dense(encoded, latent_dim , activation = "sigmoid")
 
-# this model maps an input to its reconstruction
+#Generamos el modelo de autoencoder
 # Se presenta el autoenconder entero, dentro de el codifica y decodifica
 # Donde le proporciona la entrada y el decode definido que tira del encode tambien definido
-#  Con keras_model ? se está haciendo una llamada a la API que proporiona Keras
-autoencoder <- keras_model(input_img, decoded)
+model_autoencoder <- keras_model(input, decoded)
 
 # **********************
 # Compilacion del modelo 
@@ -95,44 +103,22 @@ autoencoder <- keras_model(input_img, decoded)
 
 
 # Para compilar el modelo se necesita un optimizador y la perdida
-autoencoder  %>% compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
+model_autoencoder  %>% compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
 
 # Información sobre el autoencoder
-summary(autoencoder)
+summary(model_autoencoder)
 
 
 # **********************
 # Entrenamiento del modelo 
 # **********************
 
-history <- autoencoder %>% fit (x_train_matrix, x_train_matrix,
-                     epochs=50,
-                     batch_size=256,
-                     shuffle=TRUE,
-                     validation_data=list(x_test_matrix, x_test_matrix))
+#history <- autoencoder %>% fit(x_train_matrix, x_train_matrix,epochs=50,batch_size=256,shuffle=TRUE)
+history <- model_autoencoder %>% fit(x_train_matrix, x_train_matrix,epochs=50,batch_size=256)
 
 plot(history)
 
-####################
-# Definición de un encoded
-####################
+pesos_autoencoder <- get_weights(model_autoencoder)
+pesos_encoded <- get_weights(model_enconded)
 
-# Definición de la capa de entrada
-input_layer <- layer_input(shape = c(original_dim))
 
-# Definición de la capa codificadora
-encoded_layer<- layer_dense(input_layer,c(original_dim) , activation = "relu")
-
-# Se crea la parte codificadora
-encoder_model <- keras_model(input_layer, encoded_layer)
-
-# Compilar el modelo
-encoder_model  %>% compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
-
-# Aplicar modelo a los datos anteriormente definidos
-encoder_model %>% fit (x_train_matrix,x_train_matrix,
-                    epochs=50,
-                     batch_size=256,
-                     shuffle=TRUE,
-                     validation_data= list(x_test_matrix,x_test_matrix))
-plot(encoder_model)
